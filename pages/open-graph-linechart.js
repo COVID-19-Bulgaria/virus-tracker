2ed661d0e364f7c211f352171293f62e588faf6b
Veselin Stoyanov <5436204+st0yanov@users.noreply.github.com>
2020-04-03 01:59:40 +0300

    Added index page seo translations.

--- pages/open-graph-linechart.js
+++ pages/open-graph-linechart.js
@@ -3,8 +3,9 @@ import I18nProvider from 'next-translate/I18nProvider'
 import React from 'react'
 import C from '../pages_/open-graph-linechart'
 import ns0 from '../locales/bg/common.json'
+import ns1 from '../locales/bg/home.json'
 
-const namespaces = { 'common': ns0 }
+const namespaces = { 'common': ns0, 'home': ns1 }
 
 export default function Page(p){
   return (
