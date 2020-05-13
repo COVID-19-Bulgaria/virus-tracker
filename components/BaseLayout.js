803103c68520b2fa281351a24deb1d9296212624
Veselin Stoyanov <veselin1996@gmail.com>
2020-05-13 20:57:28 +0300

    Remove Google Analytics, Tarteaucitron. Introduced self hosted tracking pixel without cookies. Updated Privacy Policy.

--- components/BaseLayout.js
+++ components/BaseLayout.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
-
-import { useEffect } from 'react';
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
 import PropTypes from 'prop-types';
@@ -13,35 +10,6 @@ import enSEO from '../next-seo.en.config';
 const BaseLayout = ({ children }) => {
   const { lang } = useTranslation();
   const SEO = lang === 'bg' ? bgSEO : enSEO;
-  const privacyPath = lang === 'bg' ? '/privacy' : `/${lang}/privacy`;
-
-  useEffect(() => {
-    window.tarteaucitronForceLanguage = lang;
-    window.tarteaucitron.init({
-      privacyUrl: privacyPath, /* Privacy policy url */
-
-      hashtag: '#tarteaucitron', /* Open the panel with this hashtag */
-      cookieName: 'tarteaucitron', /* Cookie name */
-
-      orientation: 'bottom', /* Banner position (top - bottom) */
-      showAlertSmall: false, /* Show the small banner on bottom right */
-      cookieslist: false, /* Show the cookie list */
-
-      adblocker: false, /* Show a Warning if an adblocker is detected */
-      AcceptAllCta: true, /* Show the accept all button when highPrivacy on */
-      highPrivacy: true, /* Disable auto consent */
-      handleBrowserDNTRequest: false, /* If Do Not Track == 1, disallow all */
-
-      removeCredit: true, /* Remove credit link */
-      moreInfoLink: true, /* Show more info link */
-      useExternalCss: false, /* If false, the tarteaucitron.css file will be loaded */
-
-      readmoreLink: privacyPath, /* Change the default readmore link */
-    });
-
-    window.tarteaucitron.user.gtagUa = 'UA-137181155-2';
-    (window.tarteaucitron.job = window.tarteaucitron.job || []).push('gtag');
-  }, []);
 
   return (
     <div>
@@ -51,7 +19,6 @@ const BaseLayout = ({ children }) => {
         <link rel="stylesheet" type="text/css" href="/static/fontawesome-free/css/all.min.css" />
         <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" />
-        <script type="text/javascript" src="/static/tarteaucitronjs/tarteaucitron.js" />
       </Head>
 
       <div id="wrapper">
--- pages_/_app.js
+++ pages_/_app.js
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
 
+import { useEffect } from 'react';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import Favicon from '../components/Favicon';
 
-const CustomApp = ({ Component, pageProps }) => (
-  <>
-    <DefaultSeo {...SEO} />
-    <Favicon />
-    <Component {...pageProps} />
-  </>
-);
+const CustomApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // if (window.location.hostname === 'coronavirus-bulgaria.org') {
+    const currentPath = `?u=${encodeURIComponent(window.location.pathname)}`;
+    const referrer = document.referrer ? `&r=${encodeURIComponent(document.referrer)}` : '';
+    const analyticsPixel = new Image(1, 1);
+    analyticsPixel.src = `http://stats.coronavirus-bulgaria.org/stats.gif${currentPath}${referrer}`;
+    // }
+  }, [Component]);
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Favicon />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default CustomApp;
