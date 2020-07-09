88178f77723456d11b87e57cb1a6d496e57da1af
Veselin Stoyanov <5436204+st0yanov@users.noreply.github.com>
2020-07-10 00:44:37 +0300

    Added date positive tests percentage bar chart.

---
+++ components/PositiveTestsPercentageChart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ColumnChart } from 'react-chartkick';
+import 'chart.js';
+import useTranslation from 'next-translate/useTranslation';
+
+const PositiveTestsPercentageChart = (props) => {
+  const { data, ...rest } = props;
+  const { t } = useTranslation();
+
+  return (
+    <ColumnChart
+      data={data}
+      xtitle={t('home:charts.positive_tests_percentage_barchart.xtitle')}
+      ytitle={t('home:charts.positive_tests_percentage_barchart.ytitle')}
+      colors={['#4e73df']}
+      download={{ background: '#ffffff', filename: 'COVID-19-PositiveTestsPercentageChart' }}
+      {...rest}
+    />
+  );
+};
+
+PositiveTestsPercentageChart.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+export default PositiveTestsPercentageChart;
